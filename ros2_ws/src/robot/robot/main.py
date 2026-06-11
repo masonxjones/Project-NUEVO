de242f54701380d00748add5a51b9cd88090ab45
Josephine-rh <josephine.riverahoagland@gmail.com>
2026-06-10 20:17:06 -0700

    Update main.py

--- ros2_ws/src/robot/robot/main.py
+++ ros2_ws/src/robot/robot/main.py
@@ -15,7 +15,11 @@ from robot.path_planner import PurePursuitPlanner
 TAG_ID            = 11
 POSITION_UNIT     = Unit.MM
 WHEEL_DIAMETER    = 76.2
+<<<<<<< HEAD
+WHEEL_BASE        = 241.3
+=======
 WHEEL_BASE        = 231.3
+>>>>>>> 862462a353ec667a9a822f24d1ec9e29b0baf8eb
 INITIAL_THETA_DEG = 90.0
 
 LEFT_WHEEL_MOTOR         = Motor.DC_M1
@@ -36,7 +40,7 @@ GOAL_TOLERANCE  = 20.0    # mm
 # Arm / assembly constants
 # ---------------------------------------------------------------------------
 
-TRAFFIC_LIGHT_SWING_STEPS = -150
+TRAFFIC_LIGHT_SWING_STEPS = -100
 ELEVATOR_HOME = 0
 SWING_HOME    = 0
 
@@ -74,11 +78,11 @@ DELIVERY_LOCATIONS = {
 
 ALL_WAYPOINTS = [
     (0.0,    0.0),
-    (0.0,    3560.0),
-    (410.0,  3550.0),
-    (410.0,  810.0),
-    (1335.0, 810.0),
-    (1338.0, 3530.0),
+    (0,    3560.0),
+    (400.0,  3550.0),
+    (400.0,  790.0),
+    (1315.0, 790.0),
+    (1320.0, 3530.0),
     (2260.0, 3530.0),
     (2250.0, 0.0),
 ]
